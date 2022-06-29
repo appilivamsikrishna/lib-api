08a92ba8b04a0ed971b6c911392df0f55963334c
appilivamsikrishna <appilivamsikrishna@gmail.com>
2022-06-29 20:09:25 +0530

    1
    1

--- __test__/postAndGet.test.js
+++ __test__/postAndGet.test.js
@@ -1,73 +1,79 @@
 const request = require('../commonTests');
 const fs = require('fs');
-let userID;
+let bookID;
 
-//Create a new user
+//Create a new book
 describe("POST request", () => {
-  
-  try{
-    let userDetails;
-    beforeEach(function () {  
-        console.log("Input user details!")
-        userDetails = {
-          "name": "morpheus",
-          "job": "leader"
-      }; //new user details to be created
-      });
-    
+  try {
+    let bookDetails;
+    beforeEach(function () {
+      console.log("Input book details!");
+      bookDetails = {
+        bookTitle: "propulsion",
+        costINR: "500",
+      }; //new book details to be created
+    });
+
     afterEach(function () {
-      console.log("User is created with ID : ", userID)
+      console.log("Book is created with ID : ", bookID);
     });
 
-	  it("Create user data", async done => {
+    it("Create book data", async (done) => {
+      return (
+        request.request
+          .post(`api/books`) //post() of supertest
+          //.set('Authorization', `Token $  {request.token}`) //Authorization token
+          .send(bookDetails) //Request header
+          .expect(201) //response to be 201
+          .then((res) => {
+            expect(res.body).toBeDefined(); //test if response body is defined
+            //expect(res.body.status).toBe("success")
+            bookID = res.body.id;
+            let jsonContent = JSON.stringify({ bookID: res.body.id }); // create a json
+            fs.writeFile(
+              "data.json",
+              jsonContent,
+              "utf8",
+              function (
+                err //write book id into global json file to be used
+              ) {
+                if (err) {
+                  return console.log(err);
+                }
+                //console.log("POST response body : ", res.body)
+                done();
+              }
+            );
+          })
+      );
+    });
+  } catch (err) {
+    console.log("Exception : ", err);
+  }
+});
 
-        return request.request.post(`api/users`) //post() of supertest
-                //.set('Authorization', `Token $  {request.token}`) //Authorization token
-                .send(userDetails) //Request header
-                .expect(201) //response to be 201
-                .then((res) => {
-                    expect(res.body).toBeDefined(); //test if response body is defined
-                    //expect(res.body.status).toBe("success")
-                    userID = res.body.id;
-                    let jsonContent = JSON.stringify({userId: res.body.id}); // create a json
-                    fs.writeFile("data.json", jsonContent, 'utf8', function (err) //write user id into global json file to be used 
-                    {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    console.log("POST response body : ", res.body)
-                    done();
-                    });
-                  })
-                })
-              }
-              catch(err){
-                console.log("Exception : ", err)
-              }
+//GET all books
+describe("GET all book details", () => {
+  try {
+    beforeEach(function () {
+      console.log("GET all books details ");
+    });
+
+    afterEach(function () {
+      console.log("All book details are retrieved");
+    });
+
+    test("GET book output", async (done) => {
+      await request.request
+        .get(`api/books`) //get() of supertest
+        //.set('Authorization', `Token ${request.token}`)
+        .expect(200)
+        .then((response) => {
+          // console.log("GET RESPONSE : ", response.body);
+          done();
         });
-
-//GET all users      
-describe("GET all user details", () => {
-  
-  try{
-      beforeEach(function () {
-        console.log("GET all users details ")
     });
-          
-      afterEach(function () {
-        console.log("All users' details are retrieved")
-    });
-
-      test("GET user output", async done =>{
-        await request.request.get(`api/users`) //get() of supertest
-                                //.set('Authorization', `Token ${request.token}`) 
-                                .expect(200).then((response) =>{
-                                console.log("GET RESPONSE : ", response.body);
-                                done();
-                    })
-      })
-    }
-  catch(err){
-    console.log("Exception : ", err)
-    }
+  } catch (err) {
+    console.log("Exception : ", err);
+  }
 });
--- __test__/putAndDelete.test.js
+++ __test__/putAndDelete.test.js
@@ -6,61 +6,58 @@ const fs = require('fs'); //file system
 const data = require('../data.json'); //data.json containing the global variables
 
 
-//Update user data
-describe("PUT user details", () => {
+//Update book
+describe("PUT book details", () => {
+  try {
+    let newDetails;
+    beforeEach(function () {
+      console.log("Input updated book's details");
+      newDetails = {
+        bookTitle: "propulsion",
+        costINR: "500",
+      }; // details to be updated
+    });
+    afterEach(function () {
+      console.log("book details are updated");
+    });
 
-    try{
-        let newDetails;
-        beforeEach(function () {
-            console.log("Input updated user's details");
-            newDetails = {
-                "name": "morpheus",
-                "job": "zion resident"
-            }; // details to be updated
-  
-        });
-        afterEach(function () {
-            console.log("user details are updated");
-        });
-  
-        test("Update user now", async done =>{
+    test("Update book now", async (done) => {
+      console.log("book to be updated : ", data.bookId);
 
-            console.log("User to be updated : ", data.userId)
-
-            const response = await request.request.put(`api/users/${data.userId}`).send(newDetails) //call put() of supertest
-                                //.set('Authorization', `Token ${request.token}`) 
-                                        .expect(200)
-            expect(response.body.updatedAt).toBeDefined();
-            console.log("UPDATED RESPONSE : ", response.body);
-            done();
-    })
+      const response = await request.request
+        .put(`api/books/${data.bookId}`)
+        .send(newDetails) //call put() of supertest
+        //.set('Authorization', `Token ${request.token}`)
+        .expect(200);
+      expect(response.body.updatedAt).toBeDefined();
+      console.log("UPDATED RESPONSE : ", response.body);
+      done();
+    });
+  } catch (err) {
+    console.log("ERROR : ", err);
   }
-    catch(err){
-        console.log("ERROR : ", err)
-    }
 });
 
-//DELETE the user
-describe("DELETE user details", () =>{
-    try{
-        beforeAll(function (){
-            console.log("To delete user : ", data.userId)
-        });
+//DELETE the book
+describe("DELETE book details", () => {
+  try {
+    beforeAll(function () {
+      console.log("To delete book : ", data.bookId);
+    });
 
-        test("Delete request", async done =>{
-            const response = await request.request.delete(`api/users/${data.userId}`) //invoke delete() of supertest
-                                        .expect(204) 
-            console.log("DELETE RESPONSE : ", response.body);
-            done(); 
-        });
+    test("Delete request", async (done) => {
+      const response = await request.request
+        .delete(`api/books/${data.Id}`) //invoke delete() of supertest
+        .expect(204);
+      console.log("DELETE RESPONSE : ", response.body);
+      done();
+    });
 
-        afterAll(function (){
-            console.log("user is deleted!!")
-            //fs.unlinkSync('data.json');   //remove data.json after all tests are run    **************
-        });
-    }
-
-    catch(err){
-        console.log("EXCEPTION : ", err);
-    }
+    afterAll(function () {
+      console.log("book is deleted!!");
+      //fs.unlinkSync('data.json');   //remove data.json after all tests are run    **************
+    });
+  } catch (err) {
+    console.log("EXCEPTION : ", err);
+  }
 });
